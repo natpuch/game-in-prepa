b9adb1ebc33a5ebde5cdda5433436729fd9c71d9
Nat Puch <git@npuch.fr>
2018-05-13 00:51:28 +0200

    Added p5.play and first test

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,12 +1,31 @@
+var bat;
+var ball;
+
 function setup() {
-  // put setup code here
-  createCanvas(640, 640);
+  createCanvas(1900,1000);
+  
+  bat = createSprite(20, 200,10,100);
+  bat.setCollider("rectangle",0,0,10,100);
+  bat.mass = 5;
+  
+
+  ball = createSprite(100, 100,10,10);
+  ball.mass = 1;
+  ball.setVelocity(3,0);
 }
 
 function draw() {
-  // put drawing code here
-  fill(128);
-  ellipse(50, 50, 80, 80);
-  ellipse(25, 25, 50, 50);
+  background(0,0,0);
   
+
+  bat.position.x = mouseX;
+  bat.position.y = mouseY;
+  
+
+	
+bat.debug = mouseIsPressed;
+  
+  ball.bounce(bat);
+  
+  drawSprites();
 }
