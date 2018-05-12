b0f77fa43b97c660c3b9a0d22043a9972f89b02a
Nat Puch <git@npuch.fr>
2018-05-12 19:23:30 +0200

    Create canvas

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,9 +1,11 @@
 function setup() {
   // put setup code here
+  createCanvas(640, 480);
 }
 
 function draw() {
   // put drawing code here
   ellipse(50, 50, 80, 80);
   ellipse(25, 25, 50, 50);
+  
 }
