6ace3098050a8f14abc03f9df4c9a98f2e689462
Nat Puch <git@npuch.fr>
2018-05-14 20:30:08 +0200

    Completed level loader and added level change Added restart on left click

--- game/levels/loadLevel.js
+++ game/levels/loadLevel.js
@@ -1,14 +1,43 @@
-function loadLevel() {
+function loadLevel(level) {
+  levelPlaying = level;
   boxes = [];
   barrels = [];
   walls = [];
   grounds = [];
+  exitArray = []
+  sourcesBarrels = []
+  timeBarrels = []
 
-  loadFile();
+  loadFile(level);
 
   player = new Player(1200 * 0.1, 900 * 0.9, 1200 / 60, 1200 / 60);
-  timeBarrel = 0;
   time = 0;
+
+  console.log(sourcesBarrels);
+  timeLoad = millis();
+
+
+}
+
+function changeLevel(level) {
+  unloadLevel();
+  loadLevel(level);
+}
+
+function unloadLevel() {
+
+  for (ground of grounds) {
+    World.remove(world, ground.body);
+  }
+  for (wall of walls) {
+    World.remove(world, wall.body);
+  }
+
+  for (barrel of barrels) {
+    World.remove(world, barrel.body);
+  }
+
+  World.remove(world, player.body);
 
 }
 
@@ -33,10 +62,27 @@ function loadWall() {
           grounds.push(new Ground(eval(groundParameter[0]),eval(groundParameter[1]),eval(groundParameter[2]),eval(groundParameter[3]),eval(groundParameter[4])));
         }
       }
+      if(levela[lineNum] == "exit =") {
+        exitArray = levela[lineNum + 1].split(",");
+      }
+
+
+      if (levela[lineNum] == "barrelsSources =") {
+        sourcesArray = levela[lineNum + 1].split(";");
+
+        for (var sourcesNumber = 0 ; sourcesNumber < sourcesArray.length - 1 ; sourcesNumber++ ) {
+          sourcesParameter = sourcesArray[sourcesNumber].split(",");
+          sourcesBarrels.push([eval(sourcesParameter[0]), eval(sourcesParameter[1]),eval(sourcesParameter[2]),eval(sourcesParameter[3])]);
+          timeBarrels.push(0);
+        }
+        console.log(sourcesBarrels);
+      }
+
+
     }
     console.log("load complete");
 }
 
-function loadFile() {
-  levela = loadStrings('levels/level1.txt',loadWall);
+function loadFile(level) {
+  levela = loadStrings('levels/'+ "level" + level +'.txt',loadWall);
 }
--- game/sketch.js
+++ game/sketch.js
@@ -10,12 +10,15 @@ var grounds;
 var walls;
 var player;
 var barrels;
-var timeBarrel;
+var timeBarrels;
 var time;
+var timeLoad;
+var levelPlaying;
 
 
 var levela;
-var aaa;
+var exitArray;
+var sourcesBarrels;
 
 
 function setup() {
@@ -25,30 +28,38 @@ function setup() {
   world = engine.world;
   Engine.run(engine);
 
-  loadLevel();
+  loadLevel(1);
 
 }
 
+function mousePressed() {
+  changeLevel(levelPlaying);
+}
 
 function draw() {
   scale(width / 1200, width / 1200);
 
   background(51);
   noStroke();
-  text('EXIT', 1200 * 0.887, 900 * 0.09);
+  text('EXIT', (eval(exitArray[0])+eval(exitArray[1])) / 2 - 15, (eval(exitArray[2])+eval(exitArray[3])) / 2 - 50);
   rectMode(CENTER);
   fill(88, 41, 0);
-  rect(1200 * 0.9, 900 * 0.15, 1200 * 0.03, (1200 * 0.03) * 2.5)
+  rect((eval(exitArray[0])+eval(exitArray[1]))/2, (eval(exitArray[2])+eval(exitArray[3])) / 2, 1200 * 0.03, (1200 * 0.03) * 2.5);
   fill(255);
-  rect(1200 * 0.89, 900 * 0.15, 1200 * 0.01, (1200 * 0.01))
+  rect((eval(exitArray[0])+eval(exitArray[1]))/2 * 0.99, (eval(exitArray[2])+eval(exitArray[3])) / 2, 1200 * 0.01, (1200 * 0.01));
   player.move(millis());
   player.show();
 
-  text(millis() / 1000, 1200 / 20, 900 / 20);
+  text((millis() - timeLoad) / 1000, 1200 / 20, 900 / 20);
 
-  if (millis() - timeBarrel > 1000) {
-    barrels.push(new Barrel(1200 * 0.5, 900 * 0.1, 1200 / 80));
-    timeBarrel = millis();
+
+
+  for (var i = 0; i < sourcesBarrels.length; i++) {
+    sourceBarrels = sourcesBarrels[i];
+    if (millis() - timeBarrels[i] > sourceBarrels[3]) {
+      barrels.push(new Barrel(sourceBarrels[0], sourceBarrels[1], sourceBarrels[2]));
+      timeBarrels[i] = millis();
+    }
   }
 
   for (var rectBox of boxes) {
@@ -67,12 +78,12 @@ function draw() {
     barrel.show();
   }
 
-  if (player.body.position.x < 1200 * 0.95 && player.body.position.x > 1200 * 0.85 && player.body.position.y < 900 * 0.2 && player.body.position.y > 900 * 0.1) {
+  if (player.body.position.x < eval(exitArray[0]) && player.body.position.x > eval(exitArray[1]) && player.body.position.y < eval(exitArray[2]) && player.body.position.y > eval(exitArray[3])) {
     textSize(32);
     fill(0, 70, 128);
-    if (millis() < 12 * 1000 || time != 0) {
+    if ((millis() - timeLoad) < eval(exitArray[4]) || time != 0) {
       if (time == 0) {
-        time = millis();
+        time = (millis() - timeLoad);
       }
       text('YOU WIN !' + time / 1000 + 's', 1200 / 2, 900 / 2);
     } else {
